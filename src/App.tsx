043431f68b7f37faa002458f716fdeebadc30f29
Mger-arzi <mger54598@gmail.com>
2023-11-18 18:28:03 +0300

    фвв

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className='app-wprapper'>
+      <header className='header'>
+        <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
+      </header>
+      <nav className='nav'>
+        <div>
+          Profile
+        </div>
+        <div>
+          Mesege
+        </div>
+      </nav>
+      <div className='content'>
+        Main content
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
