f37784f205244792e1da11ba93f39027c24143b5
Mger-arzi <mger54598@gmail.com>
2023-12-11 10:41:09 +0300

    add

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import classes from './Dialogs.module.css'
+import { NavLink } from 'react-router-dom';
 export const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
                 <div className={classes.dialog}>
-                    Mger
+                    <NavLink to='/dialogs/Mger' activeClassName={classes.active}>Mger   </NavLink>
+                </div>
+
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/David' activeClassName={classes.active}> David</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    David
+                    <NavLink to='/dialogs/Violetta' activeClassName={classes.active}>Violetta</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Violetta
+                    <NavLink to='/dialogs/Andrey' activeClassName={classes.active}>Andrey</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Andrey
+                    <NavLink to='/dialogs/Casha' activeClassName={classes.active}>Casha</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Casha
+                    <NavLink to='/dialogs/Artem' activeClassName={classes.active}>Artem</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Artem
-                </div>
-                <div className={classes.dialog}>
-                    Dimych
+                    <NavLink to='/dialogs/Dimych' activeClassName={classes.active}>Dimych</NavLink>
                 </div>
             </div>
             <div className={classes.messeges}>
