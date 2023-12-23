1cd81eefb04fbb5725bde88b7e46e22adebfab8c
Mger-arzi <mger54598@gmail.com>
2023-12-23 19:23:23 +0300

    add

--- src/components/profile/MyPosts/MyPosts.tsx
+++ src/components/profile/MyPosts/MyPosts.tsx
@@ -3,11 +3,11 @@ import classes from "./MyPosts.module.css";
 import { Post } from "./Post/Post";
 import { message } from 'antd';
 export const MyPosts = () => {
-    let PostsData = [
+    let Posts = [
         { id: '1', message: 'Hello',likesCount: 12 },
         { id: '2', message: 'By by' , likesCount: 13 },
     ]
-    let postsElement = PostsData.map(el => <Post id = {el.id} message={el.message} likesCount={el.likesCount} />)
+    let postsElement = Posts.map(el => <Post id = {el.id} message={el.message} likesCount={el.likesCount} />)
 	return (
 		<div className={classes.posts}>
 			<h3>My posts</h3>
