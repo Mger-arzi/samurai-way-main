f0badadad879def698e518fd4591af2661991300
Mger-arzi <mger54598@gmail.com>
2024-01-15 19:25:06 +0300

    add

--- src/index.tsx
+++ src/index.tsx
@@ -2,30 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-let DialogsData = [
-    { id: "1", name: "Mger" },
-    { id: "2", name: "David" },
-    { id: "3", name: "Violetta" },
-    { id: "4", name: "Andrey" },
-    { id: "5", name: "Sasha" },
-    { id: "6", name: "Davit" },
-];
-let MessagesData = [
-    { id: "1", message: "Lorem, ipsum dolor.meM" },
-    { id: "2", message: "Lorem, ipsum dolor sit amet consectetur adipisicing." },
-    { id: "3", message: "Violetta" },
-    { id: "4", message: "Lorem ipsum dolor sit." },
-    { id: "5", message: "Sasha" },
-    { id: "6", message: "Davit" },
-]; 
-let Posts = [
-    { id: '1', message: 'Hello', likesCount: 12 },
-    { id: '2', message: 'By by', likesCount: 13 },
-]
+import { state } from './redux/state';
+// let DialogsData = [
+//     { id: "1", name: "Mger" },
+//     { id: "2", name: "David" },
+//     { id: "3", name: "Violetta" },
+//     { id: "4", name: "Andrey" },
+//     { id: "5", name: "Sasha" },
+//     { id: "6", name: "Davit" },
+// ];
+// let MessagesData = [
+//     { id: "1", message: "Lorem, ipsum dolor.meM" },
+//     { id: "2", message: "Lorem, ipsum dolor sit amet consectetur adipisicing." },
+//     { id: "3", message: "Violetta" },
+//     { id: "4", message: "Lorem ipsum dolor sit." },
+//     { id: "5", message: "Sasha" },
+//     { id: "6", message: "Davit" },
+// ]; 
+// let Posts = [
+//     { id: '1', message: 'Hello', likesCount: 12 },
+//     { id: '2', message: 'By by', likesCount: 13 },
+// ]
 
 
 ReactDOM.render(
-    <App DialogsData ={DialogsData} MessagesData = {MessagesData} Posts={Posts} />,
+    <App  DialogsData ={state.DialogsData} MessagesData = {state.MessagesData} Posts={state.Posts} />,
     document.getElementById('root')
 );      
---
+++ src/redux/state.tsx
@@ -0,0 +1,22 @@
+export let state = {
+    DialogsData: [
+        { id: "1", name: "Mger" },
+        { id: "2", name: "David" },
+        { id: "3", name: "Violetta" },
+        { id: "4", name: "Andrey" },
+        { id: "5", name: "Sasha" },
+        { id: "6", name: "Davit" },
+    ],
+    MessagesData:  [
+        { id: "1", message: "Lorem, ipsum dolor.meM" },
+        { id: "2", message: "Lorem, ipsum dolor sit amet consectetur adipisicing." },
+        { id: "3", message: "Violetta" },
+        { id: "4", message: "Lorem ipsum dolor sit." },
+        { id: "5", message: "Sasha" },
+        { id: "6", message: "Davit" },
+    ],
+    Posts: [
+        { id: '1', message: 'Hello', likesCount: 12 },
+        { id: '2', message: 'By by', likesCount: 13 },
+    ],
+}
