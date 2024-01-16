f75b5895acda7208e244ef6c4f8933abe99dc5ce
Mger-arzi <mger54598@gmail.com>
2024-01-16 10:16:39 +0300

    add

--- src/index.tsx
+++ src/index.tsx
@@ -3,28 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { state } from './redux/state';
-// let DialogsData = [
-//     { id: "1", name: "Mger" },
-//     { id: "2", name: "David" },
-//     { id: "3", name: "Violetta" },
-//     { id: "4", name: "Andrey" },
-//     { id: "5", name: "Sasha" },
-//     { id: "6", name: "Davit" },
-// ];
-// let MessagesData = [
-//     { id: "1", message: "Lorem, ipsum dolor.meM" },
-//     { id: "2", message: "Lorem, ipsum dolor sit amet consectetur adipisicing." },
-//     { id: "3", message: "Violetta" },
-//     { id: "4", message: "Lorem ipsum dolor sit." },
-//     { id: "5", message: "Sasha" },
-//     { id: "6", message: "Davit" },
-// ]; 
-// let Posts = [
-//     { id: '1', message: 'Hello', likesCount: 12 },
-//     { id: '2', message: 'By by', likesCount: 13 },
-// ]
 
-sdfdsf
+
 ReactDOM.render(
     <App  DialogsData ={state.DialogsData} MessagesData = {state.MessagesData} Posts={state.Posts} />,
     document.getElementById('root')
